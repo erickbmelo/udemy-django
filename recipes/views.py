9896f85e59d8998e0221b2ea1ab0c8c3e03b4f94
erick.melo <erick.melo@sorocaba.sp.gov.br>
2023-01-20 14:34:46 -0300

    Aula 28. Introdução a css e partials

--- recipes/views.py
+++ recipes/views.py
@@ -2,6 +2,6 @@ from django.shortcuts import render
 
 # Create your views here.
 def home(request):
-    return render(request, 'recipes/home.html', {
+    return render(request, 'recipes/pages/home.html', {
         'name': 'Erick Brito',
     })
